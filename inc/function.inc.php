949f652422560c4c6ebdab67f3ba0b7b6eb6192e
Prabhakar Gupta <prabhakargupta267@gmail.com>
2015-12-24 00:06:38 +0530

    get JSON response complete

--- inc/function.inc.php
+++ inc/function.inc.php
@@ -1,12 +1,29 @@
 <?php
 
-	function getSectionCode($connection, $branch, $semester, $section){
-		$query = "SELECT `id` FROM `main` WHERE `branch`='$branch' AND `sem`='$semester' AND `section`='$section' LIMIT 1";
+	function getSectionCode($connection, $branch, $semester, $section, $flag = false){
+		if($flag){
+			$query = "SELECT `id`,`last_updated_tx` FROM `main` WHERE `branch`='$branch' AND `sem`='$semester' AND `section`='$section' LIMIT 1";
+		} else {
+			$query = "SELECT `id` FROM `main` WHERE `branch`='$branch' AND `sem`='$semester' AND `section`='$section' LIMIT 1";
+		}
+		
 		$query_row = mysqli_fetch_assoc(mysqli_query($connection, $query));
 
-		return $query_row['id'];
+		if($flag){
+			return $query_row;
+		} else {
+			return $query_row['id'];
+		}
 	}
 	
 	function getTimeStamp($time){
 		return date("Y-m-d H:i:s", $time);
+	}
+
+	function addToArray($array, $element){
+		if(!in_array($element, $array)){
+			array_push($array, $element);
+		}
+
+		return $array;
 	}
