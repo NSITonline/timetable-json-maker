71811420f3fa1d201fc1480e47e17abbda69c9cd
Prabhakar Gupta <prabhakar.gupta@nsitonline.in>
2016-08-15 14:17:55 +0530

    ICE and MPAE keys inverted in GET api

--- api/get-data.php
+++ api/get-data.php
@@ -2,8 +2,8 @@
 /**
  * @Author: prabhakar
  * @Date:   2016-03-25 21:40:40
- * @Last Modified by:   Prabhakar Gupta
- * @Last Modified time: 2016-03-25 23:44:36
+ * @Last Modified by:   prabhakar
+ * @Last Modified time: 2016-08-15 14:13:53
  */
 
 $final_response = array();
@@ -13,6 +13,12 @@ if(isset($_GET['branch']) && isset($_GET['semester']) && isset($_GET['section'])
 	$branch = (int)$_GET['branch'];
 	$semester = (int)$_GET['semester'];
 	$section = (int)$_GET['section'];
+
+	if($branch == 4){
+		$branch = 5;
+	} elseif($branch == 5) {
+		$branch = 4;
+	}
 
 	$filename = array($branch, $semester, $section);
 	$filename = implode("-", $filename);
